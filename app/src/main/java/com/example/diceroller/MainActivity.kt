bca716cf2c0c9b276545375cef1f012ac7459b33
Rishav Das <rishavdas27602@gmail.com>
2022-09-25 15:17:36 +0530

    finished project

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+//            val diceImage: ImageView = findViewById(R.id.imageView)
+            rollDice()
+        }
+        // Do a dice roll when the app starts
+        rollDice()
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll the dice
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
+    }
+
+
+}
+
+fun main() {
+    val myFirstDice = Dice(6)
+    val rollResult = myFirstDice.roll()
+    val luckyNumber = 4
+
+    when (rollResult) {
+        luckyNumber -> println("You won!")
+        1 -> println("So sorry! You rolled a 1. Try again!")
+        2 -> println("Sadly, you rolled a 2. Try again!")
+        3 -> println("Unfortunately, you rolled a 3. Try again!")
+        5 -> println("Don't cry! You rolled a 5. Try again!")
+        6 -> println("Apologies! You rolled a 6. Try again!")
+    }
+}
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
